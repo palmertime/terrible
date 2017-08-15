8aab87443670f33d56671c2ae31847a5e07c7f77
palmertime <palmertime@gmail.com>
2017-08-15 15:01:05 -0700

    Initial development (#2)
    * Added slack notification to travis-ci
    
    * Initial working application
    
    * Updated basic test to pass.
    
    * futurized for py 2 and 3
    
    * Adding python 3.6 support
    
    * Added Documentation

--- terrible/cli.py
+++ terrible/cli.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 
 """Console script for terrible."""
+from __future__ import absolute_import
 
+import json
 import click
+from .terrible import (state_pull, state_resources, state_hosts, query_list,
+                       query_host)
 
 
 @click.command()
-def main(args=None):
-    """Console script for terrible."""
-    click.echo("Replace this message by putting your code into "
-               "terrible.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('--host', help='Show varibles for single host')
+@click.option('listvars', '--list', is_flag=True, help='List all variables')
+@click.option('--nometa', is_flag=True, help='Remove _meta from output')
+@click.option('--pretty', is_flag=True, help='Make json look pretty')
+@click.argument('root', metavar='<root_dir>', envvar='TERRAFORM_ROOT', default='terraform',
+                type=click.Path(exists=True))
+def main(host, listvars, nometa, pretty, root):
+    """Terrible extracts Ansible inventory data from Terraform state.
+    The <root_dir> is relative to the directory where ansible is executed but
+    defaults to ./terraform in the current directory.
+    """
+    hosts = state_hosts(state_resources(state_pull(root)))
+    if listvars:
+        output = query_list(hosts)
+        if nometa:
+            del output['_meta']
+        click.echo(json.dumps(output, indent=4 if pretty else None))
+    elif host:
+        output = query_host(hosts, host)
+        click.echo(json.dumps(output, indent=4 if pretty else None))
 
 
 if __name__ == "__main__":
--- terrible/terrible.py
+++ terrible/terrible.py
@@ -1,3 +1,126 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import json
+from collections import defaultdict
+from subprocess import (Popen, PIPE)
+
+
+def state_pull(root=None):
+    """Gather tfstate output from terraform."""
+    root = root
+    getstate = Popen(['terraform', 'state', 'pull'], cwd=root, stdout=PIPE)
+    output = getstate.stdout.read()
+    yield json.loads(output)
+
+
+def state_resources(sources):
+    for source in sources:
+        if type(source) in [str, str]:
+            with open(source, 'r') as json_file:
+                state = json.load(json_file)
+        else:
+            state = source
+            for module in state['modules']:
+                name = module['path'][-1]
+                for key, resource in list(module['resources'].items()):
+                    yield name, key, resource
+
+
+PARSERS = {}
+
+
+def state_hosts(resources):
+    '''yield host tuples of (name, attributes, groups)'''
+    for module_name, key, resource in resources:
+        resource_type, name = key.split('.', 1)
+        try:
+            parser = PARSERS[resource_type]
+        except KeyError:
+            continue
+
+        yield parser(resource, module_name)
+
+
+def parses(prefix):
+    def inner(func):
+        PARSERS[prefix] = func
+        return func
+
+    return inner
+
+
+def parse_dict(source, prefix, sep='.'):
+    return dict(_parse_prefix(source, prefix, sep))
+
+
+def _parse_prefix(source, prefix, sep='.'):
+    for compkey, value in list(source.items()):
+        try:
+            curprefix, rest = compkey.split(sep, 1)
+        except ValueError:
+            continue
+
+        if curprefix != prefix or rest == '#':
+            continue
+
+        yield rest, value
+
+
+@parses('vsphere_virtual_machine')
+def vsphere_host(resource, module_name, **kwargs):
+    raw_attrs = resource['primary']['attributes']
+    network_attrs = parse_dict(raw_attrs, 'network_interface')
+    network = parse_dict(network_attrs, '0')
+    ip_address = network.get('ipv4_address', network['ip_address'])
+    name = raw_attrs['name']
+    groups = []
+
+    attrs = {
+        'id': raw_attrs['id'],
+        'ip_address': ip_address,
+        'private_ipv4': ip_address,
+        'public_ipv4': ip_address,
+        'metadata': parse_dict(raw_attrs, 'custom_configuration_parameters'),
+        'provider': 'vsphere',
+    }
+
+    try:
+        attrs.update({
+            'ansible_ssh_host': ip_address,
+        })
+    except (KeyError, ValueError):
+        attrs.update({'ansible_ssh_host': '', })
+
+    # attrs specific to Ansible
+    if 'ssh_user' in attrs['metadata']:
+        attrs['ansible_ssh_user'] = attrs['metadata']['ssh_user']
+
+    if 'ansible_group' in attrs['metadata']:
+        groups.append(attrs['metadata'].get('ansible_group'))
+
+    return name, attrs, groups
+
+
+def query_host(hosts, target):
+    for name, attrs, _ in hosts:
+        if name == target:
+            return attrs
+
+    return {}
+
+
+def query_list(hosts):
+    groups = defaultdict(dict)
+    meta = {}
+
+    for name, attrs, hostgroups in hosts:
+        for group in set(hostgroups):
+            groups[group].setdefault('hosts', [])
+            groups[group]['hosts'].append(name)
+
+        meta[name] = attrs
+
+    groups['_meta'] = {'hostvars': meta}
+    return groups
